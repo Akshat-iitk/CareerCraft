4e98218cb8420158ea055f2e0a5f6708fe9a9e72
Akshat-iitk <akshatt21@iitk.ac.in>
2023-12-22 20:10:24 +0530

    added routes in app.jsx for different pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,47 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Footer, Navbar } from "./Components";
+
+function Layout() {
+  const user = true;
+  const location = useLocation();
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/user-auth" state={{ from: location }} replace />
+  );
+}
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main>
+      <Navbar />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={<Navigate to="/find-jobs" replace={true} />}
+          />
+          <Route path="/find-jobs" element={<Findjobs />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route
+            path={
+              user?.user?.accountType === "seeker"
+                ? "/user-profile"
+                : "/user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+
+          <Route path={"/company-profile"} element={<CompanyProfile />} />
+          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+          <Route path={"/upload-job"} element={<UploadJob />} />
+          <Route path={"/job-detail/:id"} element={<JobDetail />} />
+        </Route>
+        <Route path="/about-us" element={<About />} />
+        <Route path="/user-auth" element={<AuthPage />} />
+      </Routes>
+      {user && <Footer />} // if the user is logged in then only we will show
+      Footer component
+    </main>
   );
 }
 
