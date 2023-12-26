d350a5dceaf84db7c206f21eafe6d2bdfbda79c8
Akshat-iitk <akshatt21@iitk.ac.in>
2023-12-26 10:47:29 +0530

    Add icons and listbox to FindJobs page

--- client/src/pages/FindJobs.jsx
+++ client/src/pages/FindJobs.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Header } from "../components";
+import { CustomButton, Header } from "../components";
+import { BiBriefcaseAlt2 } from "react-icons/bi";
+import { BsStars } from "react-icons/bs";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { experience, jobTypes, jobs } from "../utils/data";
+import { Listbox } from "@headlessui/react";
 
 function FindJobs() {
-  const[sort, setSort] = useState("Newest");
+  const [sort, setSort] = useState("Newest");
   const [page, setPage] = useState(1);
   const [numPage, setNumPage] = useState(1);
   const [recordCount, setRecordCount] = useState(0);
@@ -12,18 +17,22 @@ function FindJobs() {
   const [jobLocation, setJobLocation] = useState("");
   const [filterJobTypes, setFilterJobTypes] = useState([]); //filter according to JobTypes
   const [filterExp, setFilterExp] = useState([]); //job filter according ko experience
-  const [isfFtching, setIsFetching] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-  return <div><Header
-  title='Discover Your Ideal Career Path'
-  type='home'
-  handleClick={() => {}}
-  searchQuery={searchQuery}
-  setSearchQuery={setSearchQuery}
-  location={jobLocation}
-  setLocation={setJobLocation}
-  /></div>;
+  return (
+    <div>
+      <Header
+        title="Discover Your Ideal Career Path"
+        type="home"
+        handleClick={() => {}}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        location={jobLocation}
+        setLocation={setJobLocation}
+      />
+    </div>
+  );
 }
 
 export default FindJobs;
