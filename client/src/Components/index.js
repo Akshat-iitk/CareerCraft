6fa218f831821effae3e0932467d24fd1de2ac01
Akshat-iitk <akshatt21@iitk.ac.in>
2023-12-27 23:03:07 +0530

    Implement Basic Layout for Company Page - Utilize existing components to create a Company page layout. - Display a list of registered companies on the portal. This commit introduces the initial structure for the Company page, leveraging components from the project. The page now provides a basic layout showcasing a list of registered companies.

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -6,4 +6,6 @@ import SignUp from "./SignUp";
 import Header from "./Header";
 import ListBox from "./ListBox";
 import JobCard from "./JobCard";
-export { Navbar, Footer , CustomButton , TextInput , SignUp , Header,ListBox,JobCard};
+import CompanyCard from "./CompanyCard";
+import Loading from "./Loading";
+export { Navbar, Footer , CustomButton , TextInput , SignUp , Header,ListBox,JobCard,CompanyCard,Loading};
--- client/src/pages/Companies.jsx
+++ client/src/pages/Companies.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { CompanyCard, Header, ListBox, Loading } from "../components";
+import { companies } from "../utils/data";
 
 function Companies() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [page, setPage] = useState(1);
+  const [numPage, setNumPage] = useState(1);
+  const [recordsCount, setRecordsCount] = useState(0);
+  const [data, setData] = useState(companies ?? []);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [cmpLocation, setCmpLocation] = useState("");
+  const [sort, setSort] = useState("Newest");
+  const [isFetching, setIsFetching] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleSearchSubmit = () => {};
+  const handleShowMore = () => {};
+  return <div className="w-full">
+    <Header
+        title='Find Your Dream Company'
+        handleClick={handleSearchSubmit}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        location={cmpLocation}
+        setLocation={setSearchQuery}
+      />
+       <div className='container mx-auto flex flex-col gap-5 2xl:gap-10 px-5 md:px-0 py-6 bg-[#f7fdfd]'>
+        <div className='flex items-center justify-between mb-4'>
+        <p className='text-sm md:text-base'>
+            Showing: <span className='font-semibold'>1,000+</span> Companies
+            Available
+          </p>
+          <div className='flex flex-col md:flex-row gap-0 md:gap-2 md:items-center'>
+            <p className='text-sm md:text-base'>Sort By:</p>
+
+            <ListBox sort={sort} setSort={setSort} />
+          </div>
+          </div>
+          <div className='w-full flex flex-col gap-6'>
+          {data?.map((cmp, index) => (
+            <CompanyCard cmp={cmp} key={index} />
+          ))}
+
+          {isFetching && (
+            <div className='mt-10'>
+              <Loading />
+            </div>
+          )}
+
+          <p className='text-sm text-right'>
+            {data?.length} records out of {recordsCount}
+          </p>
+        </div>
+        </div>
+  </div>;
 }
 
-export default Companies
+export default Companies;
